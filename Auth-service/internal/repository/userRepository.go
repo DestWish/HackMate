2fb3e716f41327495f2dfcb7f85db293b5636314
John1Why <SemenikhinIA23@st.ithub.ru>
2026-05-05 15:24:18 +0300

    This is my problem

--- Auth-service/internal/repository/userRepository.go
+++ Auth-service/internal/repository/userRepository.go
@@ -1,2 +1,30 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/DestWish/HackMate/Auth-service/internal/models"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db	*gorm.DB
+	redisClient *redis.Client
+}
+
+
+func NewUserRepo(db *gorm.DB, redisClient *redis.Client) *UserRepository {
+	return &UserRepository{db: db, redisClient:  redisClient}
+}
+
+
+func (r *UserRepository) userCaching(ctx context.Context, user *models.User) error {
+	key := userCacheKey(User.login)
+}
+
+
+func userCacheKey(userID uint) string {
+	return fmt.Sprintf("user:%v", userID)
+}
